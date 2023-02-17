467d4f8dee16af9f9768dd8082ea11085dcfe227
Pratik-Nawale <pratiknawale03@gmail.com>
2023-02-17 09:26:47 +0530

    add authentication to create new post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const checkAuthentication = require('../config/passport-local-startegy')
+
+
 
 const postsController = require("../controllers/posts_controller");
 
-router.post("/create", postsController.create);
+router.post("/create", checkAuthentication, postsController.create);
 
 module.exports = router;
